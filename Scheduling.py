f7ba6554560b37c187eb3673781215a1b8326169
Triet Vo <vominhtriet02@gmail.com>
2020-12-08 17:38:03 -0500

    Better schedule viewing

--- Scheduling.py
+++ Scheduling.py
@@ -1,7 +1,5 @@
 from AlbertCourse import *
 from WeekSchedule import *
-from Day import *
-from Classes import *
 class Scheduling:
     def __init__(self,all_courses):
         self.all_classes = []
@@ -29,13 +27,47 @@ class Scheduling:
 def main():
 
     cs2124 = AlbertCourse("CS 2314")
-    cs2124.add_lab([(1,8,11),(1,12,15)])
-    cs2124.add_lecture([(4,12,13.5),(4,14,16)])
+    cs2124.add_lecture([(0,9.50,10.83),(2,9.50,10.83)])
+    cs2124.add_lecture([(0,11,12.33),(2,11,12.33)])
+    cs2124.add_lab([(4,8,10.83)])
+    cs2124.add_lab([(4,11,13.83)])
+    cs2124.add_lab([(4,14,16.83)])
+
     ma2314 = AlbertCourse("MA 2314")
-    ma2314.add_lab([(3,8,11),(3,12,15)])
-    all_courses = [cs2124,ma2314]
-    sched = Scheduling(all_courses)
+    ma2314.add_lecture([(0,9.50,10.83),(2,9.50,10.83)])
+    ma2314.add_reci([(3,10,10.83)])
+    ma2314.add_reci([(3,11,11.83)])
+    ma2314.add_reci([(3,14,14.83)])
+    ma2314.add_reci([(3,15,15.83)])
+
+    exposua2 = AlbertCourse("EXPOS-UA 2")
+    exposua2.add_lecture([(1,8,9.25),(3,8,9.25)])
+
+    cs1122 = AlbertCourse("CS 1122")
+    cs1122.add_lab([(4,9,10.83)])
+
+    ph1013 = AlbertCourse("PH 1013")
+    ph1013.add_lecture([(0,15.5,17),(2,15.5,17)])
+    ph1013.add_reci([(4,14,15)])
+
+    sched = Scheduling([cs2124,ph1013,cs1122,exposua2,ma2314])
     sched.backtracking()
-    for i in sched.all_arrangements:
-        print(i)
+    i = 0
+    n = len(sched.all_arrangements)
+    while True:
+        print(sched.all_arrangements[i])
+        while True:
+            string = input("D to see next schedule, A to see previous schedule, X to exit\n")
+            if string.lower() == "d":
+                if i+1 < n:
+                    i+=1
+                    break
+            if string.lower() == "a":
+                if i-1>-1:
+                    i-=1
+                    break
+            if string.lower() == "x":
+                return
+
+
 main()
